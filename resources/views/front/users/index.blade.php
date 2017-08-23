818a5abe7e39fc99a453d81f164004ffef905e38
Munzir Rosdi <munzir_hazazi@meteor.com.my>
2017-08-23 23:11:47 +0800

    asadg

--- resources/views/front/users/index.blade.php
+++ resources/views/front/users/index.blade.php
@@ -18,7 +18,7 @@
             <tr>
                 <td width="25%">{{$user->name}}</td>
                 <td width="30%">{{$user->email}}</td>
-                <td width="30%">{{$user->roles->first()->name}}</td>
+                <td width="30%">{{ isset($user->roles) ? $user->roles->first()->name : null }}</td>
                 <td>
                     <a href="{{route('users.edit',$user->id)}}"><i class="icon ion-edit text-warning mr-2"></i></a>
                     <a href="#" data-toggle="delete" class="text-danger"><i class="icon ion-close-circled"></i></a>
