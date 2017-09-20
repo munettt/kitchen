c22f622aeebc49ec46771be2982aa1c40ffd786f
Munzir Rosdi <munzir_hazazi@meteor.com.my>
2017-09-21 07:32:52 +0800

    responsive fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,29 +19,31 @@
         <div class="sidebar col-12 col-md-2 col-lg-1">
             <div class="site-brand"><a href="{{url("/")}}"><img src="{{asset('images/fork.png')}}" alt="logo"></a></div>
             <ul class="nav nav-pills sidebar-nav x-scrollable">
-                <li class="nav-item"><a class="nav-link {{$nav=='dashboard'?'active':''}}" href="{{url('/')}}"><em class="icon ion-speedometer"></em> Dashboard <span class="sr-only">(current)</span></a></li>
+                <li class="nav-item"><a class="nav-link {{$nav=='dashboard'?'active':''}}" href="{{url('/')}}"><i class="icon ion-speedometer"></i> Dashboard <span class="sr-only">(current)</span></a></li>
                 <li class="nav-item"><a class="nav-link  {{$nav=='apps'?'active':''}}" href="{{url('/apps')}}"><i class="icon ion-android-cloud-outline"></i> Applications</a></li>
-                <li class="nav-item"><a class="nav-link  {{$nav=='commands'?'active':''}}" href="{{url('/commands')}}"><em class="icon ion-clipboard"></em> Commands</a></li>
-                <li class="nav-item"><a class="nav-link  {{$nav=='backup'?'active':''}}" href="{{url('/backup')}}"><em class="icon ion-lock-combination"></em> Backup</a></li>
+                <li class="nav-item"><a class="nav-link  {{$nav=='commands'?'active':''}}" href="{{url('/commands')}}"><i class="icon ion-clipboard"></i> Commands</a></li>
+                <li class="nav-item"><a class="nav-link  {{$nav=='backup'?'active':''}}" href="{{url('/backup')}}"><i class="icon ion-lock-combination"></i> Backup</a></li>
                 @role('admin')
-                <li class="nav-item"><a class="nav-link  {{$nav=='users'?'active':''}}" href="{{url('/users')}}"><em class="icon ion-person-stalker"></em> Users</a></li>
+                <li class="nav-item"><a class="nav-link {{$nav=='users'?'active':''}}" href="{{url('/users')}}"><i class="icon ion-person-stalker"></i> Users</a></li>
                 @endrole
+                <li class="nav-item d-sm-none"><a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="icon ion-unlocked"></i> Logout</a></li>
             </ul>
         </div>
         <div class="col-12 col-lg-11 col-md-10 ml-auto">
             <header class="row justify-center">
-                <div class="col-md-6 col-lg-8">
-                    <h6 class="badge badge-warning">{{config('app.name')}}</h6>
+                <div class="col-9 col-md-6 col-lg-8">
+                    {{--<h6 class="badge badge-warning">{{config('app.name')}}</h6>--}}
                     @yield('page-title')
                 </div>
-                <div class="col-md-6 col-lg-4 text-md-right pt-3 pt-md-0">
+                <div class="col-3 col-md-6 col-lg-4 text-left text-md-right d-none d-md-block">
                     <div class="dropdown dropdown-user">
 
-                        <a href="#" class="btn btn-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <a href="#" class="btn btn-link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <div class="d-inline-block avatar">
                                 <img src="{{auth()->user()->social->avatar}}" alt="" class="rounded-circle">
+                                <span class="d-none d-md-inline ml-2">{!! auth()->user()->email !!}</span>
                             </div>
-                            <span class="d-none d-md-inline">{!! auth()->user()->email !!}</span>
+
                         </a>
                         <div class="dropdown-menu dropdown-menu-right">
                             <div class="dropdown-item"><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></div>
